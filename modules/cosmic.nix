d2dd08d07dc314702f26276716ba339f6b290af2
JulianPasco <julian@hotstuff.co.za>
2025-11-29 11:00:06 +0200

    Fix renamed noto-fonts-emoji package

--- modules/cosmic.nix
+++ modules/cosmic.nix
@@ -130,7 +130,7 @@ in
     packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       liberation_ttf
       fira-code
       fira-code-symbols
